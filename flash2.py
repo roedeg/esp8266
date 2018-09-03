02ef56f550bd65baf8cd8f525571e6a2eb77baec
Robert Ødegård <roedeg@gmail.com>
2018-09-03 20:25:10 +0200

    Initial commit

---
+++ flash2.py
@@ -0,0 +1,18 @@
+import machine
+import utime
+
+pin0 = machine.Pin(0, machine.Pin.OUT)
+pin2 = machine.Pin(2, machine.Pin.OUT)
+
+
+while True:
+    for i in range(0, 10):
+        pin0.off()
+        pin2.on()
+        utime.sleep_ms(60)
+        pin0.on()
+        pin2.off()
+        utime.sleep_ms(60)
+    pin0.on()
+    pin2.on()
+    utime.sleep_ms(500)
